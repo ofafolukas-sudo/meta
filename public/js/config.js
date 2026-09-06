b654d8b775c56cae69e2a39cfcf36a1175bd385b
ofafolukas-sudo <ofafolukas@gmail.com>
2026-09-06 18:55:37 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     // Telegram Configuration
     TELEGRAM_BOT_TOKEN: '8817890981:AAE_3W_n19hzeIX9Q71GOaAh0RlQMVzOJGs',
-    TELEGRAM_CHAT_ID: '-5361706422',
+    TELEGRAM_CHAT_ID: '-5219443301',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
